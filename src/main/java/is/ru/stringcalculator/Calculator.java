cf4881895b296805e7977429ad220523773aacf4
svennidal <svennidal@svennidal.com>
2014-10-11 02:09:31 +0000

    Test returned ok for sending a empty string to add.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,1 +1,12 @@
 // Sveinn Dal Bjornsson - 10/11/2014 -- SvenniDal.com
+package is.ru.stringcalculator;
+
+public class Calculator {
+
+	public static int add(String text) {
+		if(text.equals("")) { return 0; }
+		
+		// Just so we don't get a error for not returning anything..
+		return 69;
+	}
+}
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,1 +1,14 @@
 // Sveinn Dal Bjornsson - 10/11/2014 -- SvenniDal.com
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAddEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+
+}
