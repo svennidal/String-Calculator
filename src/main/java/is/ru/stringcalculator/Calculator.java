24734d36a45d1fd59796c6f3de5bf90ac8315c72
svennidal <svennidal@svennidal.com>
2014-10-11 02:37:17 +0000

    3 private functions added. Testing them publicly frowned apon.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,10 +3,37 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
+	// The add function of the calculator.
 	public static int add(String text) {
+		// Empty string.
 		if(text.equals("")) { return 0; }
+		// Adding two or more numbers together.
+		else if(text.contains(",")) { return sum(splitNos(text)); }
 		
 		// Just so we don't get a error for not returning anything..
 		return 69;
 	}
+
+	// Parse the string as an integer.
+	private static int strInt(String no) {
+		return Integer.parseInt(no);
+	}
+
+	// Split string int an array of string, splitting elements at ",". 
+	private static String[] splitNos(String nos) {
+		return nos.split(",");
+	}
+
+	// Return a sum of all elements in an array
+	private static int sum(String[] nos) {
+		int total = 0;
+		for(String no : nos) {
+			total += strInt(no);
+		}
+		return total;
+	}
+
+
+
+
 }
